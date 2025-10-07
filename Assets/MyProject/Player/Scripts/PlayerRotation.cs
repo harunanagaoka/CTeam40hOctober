07ba8d3c0cf9771b0f965c12cc1b59b7e2a2d8bf
MSI\litel <iayumi6052@gmail.com>
2025-10-07 12:25:19 +0900

    プレイヤー向きの調整、タグの追加

--- Assets/MyProject/Player/Scripts/PlayerRotation.cs
+++ Assets/MyProject/Player/Scripts/PlayerRotation.cs
@@ -1,19 +1,25 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Windows;
 
 public class PlayerRotation : MonoBehaviour
 {
+    [Header("PlayerName")]
+    [SerializeField] private int m_playerName = 1; // プレイヤー名
     [Header("Rotation Settings")]
     [SerializeField] private float m_rotationSpeed = 90f;       // 回転スピード（度/秒）
-    [SerializeField] private float m_maxRotationAngle = 45f;    // 最大回転角度（左右）
+    [SerializeField] private float m_maxRotationAngle = 45f;    // 最大回転角度（左右） 
+
     PlayerInput m_input = null;
 
+    private float m_currentYaw = 0f;         // 現在の角度
     private int m_rotateDir = 0;        // -1 = 左, 1 = 右, 0 = 停止
-    private float m_currentYaw = 0f;    // 現在の回転角度
+    
 
     private void Start()
     {
+        
         m_input = GetComponent<PlayerInput>();
     }
 
@@ -29,10 +35,17 @@ public class PlayerRotation : MonoBehaviour
             m_rotateDir = 1;
         }
 
-        // 押している間だけ角度を増減
-        m_currentYaw += m_rotateDir * m_rotationSpeed * Time.deltaTime;
-        m_currentYaw = Mathf.Clamp(m_currentYaw, -m_maxRotationAngle, m_maxRotationAngle);
-
+        // 押している間だけ角度を増減(プレイヤー１の時)
+        if (m_playerName == 1)
+        {
+            m_currentYaw += m_rotateDir * m_rotationSpeed * Time.deltaTime;
+            m_currentYaw = Mathf.Clamp(m_currentYaw, -m_maxRotationAngle, m_maxRotationAngle);
+        }
+        else if (m_playerName == 2) // プレイヤー２の時
+        {
+            m_currentYaw += m_rotateDir * m_rotationSpeed * Time.deltaTime;
+            //m_currentYaw = Mathf.Clamp(m_currentYaw, -m_maxRotationAngle-180, m_maxRotationAngle+180);
+        }
 
         // 毎フレーム回転を適用
         transform.localRotation = Quaternion.Euler(0f, transform.localRotation.y + m_currentYaw, 0f);
